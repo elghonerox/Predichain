62a88df521f6c0c927190ccded4280e66dfd40f3
elghonerox <hamzaelghomri@gmail.com>
2025-11-18 03:03:25 +0100

    Configure webpack and experimental options in Next.js

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    // Disable turbopack for now, use webpack
+    turbo: undefined,
+  },
+  webpack: (config, { isServer }) => {
+    // Exclude test files and problematic modules
+    config.module = config.module || {};
+    config.module.rules = config.module.rules || [];
+    
+    config.module.rules.push({
+      test: /node_modules\/thread-stream\/(test|bench)/,
+      use: 'null-loader',
+    });
+
+    // External modules that shouldn't be bundled
+    if (!isServer) {
+      config.externals = config.externals || [];
+      config.externals.push('pino', 'thread-stream');
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
